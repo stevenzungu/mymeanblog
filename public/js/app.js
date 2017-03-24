84ba17f454d98253888958b68f75bd90404a3e6c
Mac <Mac@Macs-Air.itpro.tv>
2017-03-24 19:19:30 -0400

    implemented angular route

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,24 @@
 (function() {
-  angular.module('mymeanblog', []);  
+  angular.module('mymeanblog', [
+    "ngRoute",
+  ]);
+
+
+
+angular.module('mymeanblog')
+        .config(RouterConfig);
+
+RouterConfig.$inject = ['$routeProvider'];
+
+function RouterConfig($routeProvider){
+  $routeProvider
+    .when('/',{
+      controller: 'SignupController',
+      templateUrl: 'html/views/signup.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+   });
+}
+
 }());
