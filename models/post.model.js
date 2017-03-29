cd9f4ae656a418f72ee0f0d6967b7cbc61d83309
Mac <Mac@Macs-Air.itpro.tv>
2017-03-29 18:42:38 -0400

    added author field to the post schems and added a hook to update

--- models/post.model.js
+++ models/post.model.js
@@ -16,7 +16,15 @@ var postSchema = mongoose.Schema({
   updated: {
     type: Date,
     default: Date.now
+  },
+  author: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
   }
+});
+
+postSchema.pre('findOneAndUpdate', function(){
+  this.update({}, {$set: { updated: Date.now() }});
 });
 
 var Post = mongoose.model('Post', postSchema);
