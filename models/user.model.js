90bb08d2315ae9413f358c69c6e41e5b7be665bb
Mac <Mac@Macs-Air.itpro.tv>
2017-03-29 20:22:20 -0400

    we implemented the setPassword, validPassword, and generateJwt functions on the user model. We added the node module jsonwebtoken to the dependencies for our application

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const signature = process.env.SIGNATURE || require('../secrets').SIGNATURE;
 
-var userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   email:{
     type: String,
     required: true,
@@ -19,9 +22,24 @@ hash: {
 });
 
 
-userSchema.methods.setPassword = function(password){}
-userSchema.methods.validPassword = function(password){}
-userSchema.methods.generateJwt = function(){}
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+}
+userSchema.methods.validPassword = function(password){
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+  return this.hash === hash;   //if they equal, ye shall pass, otherwise, get out
+}
+userSchema.methods.generateJwt = function(){
+  const expiration = new Date();
+  expiration.setDate(expiration.getDate() + 7);
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiration.getTime() / 1000)
+  }, signature);
+}
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
